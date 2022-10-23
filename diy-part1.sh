cad33efcc6f5cd9ded5847ea61af46d7eda7cb29
qzyuebing <54806519+qzyuebing@users.noreply.github.com>
2022-10-23 10:04:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,17 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git smpackage https://github.com/kenzok78/small-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
+sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
 sed -i '$a src-git mmdvm https://github.com/qzyuebing/mmdvm-openwrt' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git ssd1306 https://github.com/qzyuebing/hack-ssd1306-openwrt' feeds.conf.default
-#sed -i '$a src-git OLED https://github.com/hallard/ArduiPi_OLED' feeds.conf.default
