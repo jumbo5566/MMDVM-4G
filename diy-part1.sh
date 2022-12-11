d53e2c61310e5a3e044fd7241d8366b6905178d6
sgyuebing <108126059+sgyuebing@users.noreply.github.com>
2022-12-11 09:31:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,4 +22,4 @@ sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.d
 sed -i '$a src-git smpackage https://github.com/kenzok78/small-package' feeds.conf.default
 sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
 sed -i '$a src-git mmdvm https://github.com/lazywalker/mmdvm-openwrt' feeds.conf.default
-sed -i '$a src-git OLED https://github.com/hallard/ArduiPi_OLED' feeds.conf.default
+sed -i '$a src-git OLED https://github.com/hallard/ArduiPi_SSD1306' feeds.conf.default
